2a00a4bd80c910da4fbe3cc334cc1bfc1828606b
Iago de Oliveira Silvestre <32106546+iagosilvestre@users.noreply.github.com>
2024-05-20 10:02:44 -0300

    Initial commit

---
+++ tmux/demo/AgentNode.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+import rospy
+import rospkg
+import random
+from gazebo_msgs.srv import SpawnModel, DeleteModel
+from geometry_msgs.msg import Pose
+from std_msgs.msg import Int32,Float64
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Int8
+from mrs_msgs.srv import PathSrv,PathSrvRequest
+from mrs_msgs.msg import Reference,Path
+import threading
+
+class WaypointTrackerNode:
+    def __init__(self):
+        rospy.init_node('waypoint_tracker_node', anonymous=True)
+        
+        # Parameters
+        self.threshold = 0.5
+        
+        # Initialize last waypoint index
+        self.last_waypoint_index = -1
+        
+        # Subscribers
+        rospy.Subscriber("uav1/ground_truth", Odometry, self.odometry_callback)
+        
+        # Publisher
+        self.last_waypoint_pub = rospy.Publisher("uav1_lastWP", Int8, queue_size=1)
+        
+        # Rate for the odometry callback
+        self.rate = rospy.Rate(1)  # Execute once per second
+        self.waypoints = [(-5.0, -5.0), (5.0, -5.0), (5.0, 5.0), (-5.0, 5.0)]
+        self.delete_model = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
+        self.subscriber_del = rospy.Subscriber('/fightFire', Int32, self.del_callback)
+        self.subscriber_path1 = rospy.Subscriber('/uav1/trajectory_generation/path', Path, self.path1_callback)
+        # Get the path to the SDF file
+        self.count=0
+        #self.createForest()
+
+    def path1_callback(self, msg):
+        self.waypoints = [(point.position.x, point.position.y) for point in msg.points]
+        for i, (x, y) in enumerate(self.waypoints):
+            rospy.loginfo("Waypoint %d: x=%.2f, y=%.2f", i, x, y)
+        #global points_uav1
+        #points_uav1 = msg.points
+        #print("Received points list with length:", len(points_uav1))
+        #print("Points list:")
+        #for i, point in enumerate(points_uav1):
+        #    print("Point", i, ":", point)
+
+
+    def del_callback(self, msg):
+        if msg.data != 0:
+            model_name = "tree_red_"+ str(self.count)  # Change this to the name of the model you want to delete
+            rospy.loginfo("Deleting model: %s", model_name)
+            self.count=self.count+1
+            probability = random.random()
+            if probability <= 0.3:
+                try:
+                    response = self.delete_model(model_name)
+                    rospy.loginfo("Model deletion response: %s", response.status_message)
+                except rospy.ServiceException as e:
+                    rospy.logerr("Service call failed: %s", e)
+        
+    def odometry_callback(self, msg):
+        # Extract drone position
+        x = msg.pose.pose.position.x
+        y = msg.pose.pose.position.y
+        
+        # Calculate distance to next waypoint
+        next_waypoint_index = (self.last_waypoint_index + 1) % len(self.waypoints)
+        next_waypoint_x, next_waypoint_y = self.waypoints[next_waypoint_index]
+        distance = ((x - next_waypoint_x) ** 2 + (y - next_waypoint_y) ** 2) ** 0.5
+        
+        #print("distance = %.2f"%distance)
+        # Check if drone has passed the next waypoint
+        if distance < self.threshold:
+            # Update last waypoint index
+            self.last_waypoint_index = next_waypoint_index
+            
+            # Publish last waypoint index
+            self.last_waypoint_pub.publish(self.last_waypoint_index+1)
+
+
+if __name__ == '__main__':
+    try:
+        waypoint_tracker_node = WaypointTrackerNode()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
---
+++ tmux/demo/Jason/src/agt/autopilot.asl
@@ -0,0 +1,68 @@
+last_mission("None").
+current_mission("Idle").
+progress(pa,0).
+progress(pb,0).
+progress(pc,0).
+
+//+uav_lastWP(N) : current_mission(CM) <- -+progress(CM,N).
+
+
+
++!stop_mission
+   <- .drop_intention(run_plan(_)).
+
++!run_plan(L)[source(Ag)]
+   : current_mission(CM)
+   & progress(CM,LX) & (LX == .length(L))
+   //& progress(CM,LX) & .delete(-1,LX,L,SL) & (LX == .length(SL))
+   <- .print("Finished Mission :", CM);
+      .send(Ag,signal,finished).
+
++!run_plan(L)[source(Ag)] 
+   : my_number(N)
+   & current_mission(CM) & last_mission(LM) & not(CM == LM)
+   & progress(CM,LX) & not (LX == .length(L))
+   <- .delete(-1,LX,L,SL);
+      embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("sample_roscore","test_mrs_topic_action_light",[N,SL] );
+      .print("SubList :",SL);
+      .print("SubList Size :",.length(SL));
+      -+last_mission(CM);
+      !run_plan(SL)[source(Ag)].
+
++!run_plan(L)[source(Ag)] 
+   : current_mission(CM) & last_mission(LM) & (CM == LM)
+   & progress(CM,LX) & not (LX == .length(L))
+   <- .print("Current Mission :",CM," Current Progress :",LX," List Length :",.length(L));
+      .wait(1000);
+      !run_plan(L)[source(Ag)].
+
+
++update_current_mission(Id)[source(Ag)] 
+  <- -current_mission(_);
+     +current_mission(Id);
+     -update_current_mission(Id)[source(Ag)].
+
+
++uav_lastWP(N) 
+   : current_mission(CM) 
+   <- -progress(CM,_);
+      +progress(CM,N).
+
++!do(Step,Ag,Rem)
+   <- .print("doing ",Step);
+      UsedEnergy = 5;
+      .send(Ag,achieve,update_rem_plan(Step,UsedEnergy));
+      .wait(1000);
+   .   
+
+//+!run_plan([Step|T])[source(Ag)]
+//   <- !do(Step,Ag,T); !run_plan(T)[source(Ag)].
+
+/*!consume_energy(1000). // it initially has 1000 of energy
+
++!consume_energy(E)    // that decais 10 units each second
+   <- .send(sample_agent,achieve,update_energy(E));
+      .wait(1000);
+      !consume_energy(E-10).
+*/
+//+!run_plan(_)[source(Ag)] .
