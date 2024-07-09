295be442f8c015f0ff7c51664238efe2a4705d4c
iago A&V room <iagosilvestrehp@hotmail.com>
2024-07-10 00:14:19 +0100

    improvements on making 2 paths same script

--- tmux/path_test/Tplan_path.py
+++ tmux/path_test/Tplan_path.py
@@ -17,6 +17,7 @@ class Goto:
         self.path_pub = rospy.Publisher('/uav1/trajectory_generation/path', Path, queue_size=1)
 
         path_msg = Path()
+        path_msg2= Path()
         
         path_msg.header.frame_id = ""
         path_msg.use_heading = True
@@ -24,8 +25,9 @@ class Goto:
         path_msg.max_execution_time = 5.0
         path_msg.max_deviation_from_path = 0.0
         path_msg.dont_prepend_current_state = False
-        path_msg2 = path_msg
 
+        path_msg2 = path_msg
+        #path_msg2.header.frame_id = "2" 
         for i in range(0, 11):
             point = Reference()
             point.position.x = i * 10
@@ -33,7 +35,15 @@ class Goto:
             point.position.z = 7
             point.heading = -1.571
             path_msg.points.append(point)
-        
+
+        for k in range(0, 11):
+            point = Reference()
+            point.position.x = -k * 10
+            point.position.y = 0
+            point.position.z = 7
+            point.heading = 1.571
+            path_msg2.points.append(point)
+
 
         try:
             time.sleep(1)   
