0104254521d8114a98467bccf72952bfa3b01f00
iago A&V room <iagosilvestrehp@hotmail.com>
2024-07-09 23:46:57 +0100

    Tplan_path improvements, needed sleep before publishing

--- tmux/path_test/1plan_path.py
+++ tmux/path_test/1plan_path.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 
 import rospy
+import rosnode
 import random
 import os
-import time
 
 from mrs_msgs.msg import Reference, Path
 
-class Goto:
+class planPath:
 
     def __init__(self):
-        rospy.init_node('goto', anonymous=True)
-        rospy.loginfo('ROS not initialized')
+        rospy.init_node("path1", anonymous=True)
 
         # Publisher for path
-        self.path_pub = rospy.Publisher('/uav1/trajectory_generation/path', Path, queue_size=1)
+        self.path_pub = rospy.Publisher("/uav1/trajectory_generation/path", Path, queue_size=1)
 
         path_msg = Path()
         
@@ -24,35 +23,27 @@ class Goto:
         path_msg.max_execution_time = 5.0
         path_msg.max_deviation_from_path = 0.0
         path_msg.dont_prepend_current_state = False
-        path_msg2 = path_msg
 
-        for i in range(0, 11):
+
+        for i in range(0, 10):
             point = Reference()
             point.position.x = i * 10
             point.position.y = 0
             point.position.z = 7
             point.heading = -1.571
             path_msg.points.append(point)
-        
 
         try:
-            time.sleep(1)   
             path_msg.header.stamp = rospy.Time.now()
-            rospy.loginfo('teste')
+            print(path_msg)
             self.path_pub.publish(path_msg)
-            
-            
-            
-            #path_msg2.header.stamp = rospy.Time.now()
-            #rospy.loginfo('teste2')
-            #self.path_pub.publish(path_msg2)
-
-        except rospy.ROSException as e:
-            rospy.logerr('[SweepingGenerator]: Exception occurred: {}'.format(e))
+        except:
+            rospy.logerr('[SweepingGenerator]: path topic not available')
+            pass
 
 if __name__ == '__main__':
     try:
-        goto = Goto()
+        path1 = planPath()
         rospy.spin()  # keep the node alive
     except rospy.ROSInterruptException:
         pass
